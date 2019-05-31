960480c9b901892e5a731452e57f649178691e0d
CJYjimmy <chen.6494@osu.edu>
2019-05-31 22:05:08 +0800

    add 'create new post' view

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Button, ListItem, Grid } from '@material-ui/core';
+import { Typography, Button, ListItem, Grid, TextField } from '@material-ui/core';
 import {  } from 'react-bootstrap'
 import './component_style/MainPage.css';
 import { navigate } from 'react-mini-router';
@@ -21,11 +21,21 @@ export default class MainPage extends React.Component {
         this.state = {
             createPost: false,
             showPosts: true,
+            postTitle: '',
+            postContent: '',
         }
         this.helpChild = React.createRef();
         this.handleCreatePostOpen =  this.handleCreatePostOpen.bind(this);
         this.handleCreatePostClose = this.handleCreatePostClose.bind(this);
         this.logout = this.logout.bind(this);
+    }
+
+    handleEventChange = field => event => {
+        const oldData = this.state;
+        oldData[field] = event.target.value;
+        this.setState({
+            newUserData: oldData,
+        });
     }
 
     handleCreatePostOpen = () => {
@@ -56,7 +66,7 @@ export default class MainPage extends React.Component {
                 <br />
                 <HelpView ref={this.helpChild} />
                 <Grid container justify="center" spacing={24}>
-                    <Grid item xs={7} sm={7}>
+                    <Grid item sm={7}>
                         {
                             this.state.createPost === true && (
                                 <form className="grid" method="post" action="">
@@ -64,7 +74,30 @@ export default class MainPage extends React.Component {
                                         <legend>
                                             New Post
                                         </legend>
-                                        <h2>Post Title</h2>
+                                        <h2>Post Title:</h2>
+                                        <TextField
+                                            required
+                                            label="Post Title"
+                                            variant="outlined"
+                                            value={this.state.postTitle}
+                                            className="postTitleText"
+                                            onChange={this.handleEventChange('postTitle')}
+                                        />
+                                        <br />
+                                        <h2>Content:</h2>
+                                        <textarea
+                                            name="message"
+                                            className="textarea"
+                                            rows="10"
+                                            cols="30"
+                                            wrap="soft"
+                                            value={this.state.postContent}
+                                            onChange={this.handleEventChange('postContent')}
+                                        >
+                                        </textarea>
+                                        <br/><br/>
+                                        <Button variant="contained" color="secondary" className="postButtons">Cancel</Button>
+                                        <Button variant="contained" color="primary" className="postButtons">Post</Button>
                                     </fieldset>
                                 </form>
                             )
